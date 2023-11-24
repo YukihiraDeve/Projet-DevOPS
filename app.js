f5d1faa3267f2754146dd4b16ef373166384778e
YukihiraDeve <valentinbourpro@gmail.com>
2023-11-24 16:53:09 +0100

    feat : express good

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bonjour depuis le serveur Express!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur en écoute sur le port ${PORT}`));
