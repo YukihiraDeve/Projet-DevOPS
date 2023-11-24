ee752b62bd6558fc27b3b46aa845cfc34526e97a
YukihiraDeve <valentinbourpro@gmail.com>
2023-11-24 16:57:03 +0100

    feat : express good

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config()
 
-const app = express();
+const app = express( );
 
 app.get('/', (req, res) => {
   res.send('Bonjour depuis le serveur Express!');
